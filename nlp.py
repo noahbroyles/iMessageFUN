409485ba30629751785f4ad9b071e753a72a8647
Noah Broyles <noah@javamate.net>
2020-04-30 16:01:29 -0400

    The fewer lines - the better 😉

--- nlp.py
+++ nlp.py
@@ -6,26 +6,22 @@ def getVerses(path):
     with open(path, 'r') as f:
         kjv = f.read()
     pattern = re.compile(r"(\d+:\d+.*?)\n\n", re.MULTILINE | re.DOTALL)
-    verses = [x.replace("\n", ' ') for x in pattern.findall(kjv)]
-    return verses
+    return [x.replace("\n", ' ') for x in pattern.findall(kjv)]
 
 
 def getWords(path):
     with open(path, 'r') as f:
         text = f.read()
-    words = [word.replace("\n", ' ') for word in text.split(' ')]
-    return words
+    return [word.replace("\n", ' ') for word in text.split(' ')]
 
 
 def getSentences(path):
     with open(path, 'r') as f:
         text = f.read()
-    sentences = [s.replace("\n", ' ') for s in tokenize.sent_tokenize(text)]
-    return sentences
+    return [s.replace("\n", ' ') for s in tokenize.sent_tokenize(text)]
 
 
 def getLines(path):
     with open(path, 'r') as f:
         text = f.read()
-    lines = [x.replace("\n", '') for x in text.split("\n")]
-    return lines
+    return [x.replace("\n", '') for x in text.split("\n")]
