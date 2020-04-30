0ee4b56e9b0ff73ddf9dbe6ccbe230c250fbe29f
Noah Broyles <noah@javamate.net>
2020-04-30 15:52:26 -0400

    Special options update 🔥

--- iMessageSpam.py
+++ iMessageSpam.py
@@ -6,11 +6,11 @@ def runAppleScript(applescript, verbose=False):
     args = [item for x in [("-e", l.strip()) for l in applescript.split('\n') if l.strip() != ''] for item in x]
     proc = subprocess.Popen(["osascript"] + args, stdout=subprocess.PIPE)
     progname = proc.stdout.read().strip()
-    if verbose:
-        print(str(progname))
+    # print(str(progname))
 
 
 def sendList(listOfStrings: list, appleIDorPhone: str, verbose=False):
+    num = 1
     for m in listOfStrings:
         # Leyla is the inspiration for this program, FYI ;)
         script = '''
@@ -22,21 +22,30 @@ def sendList(listOfStrings: list, appleIDorPhone: str, verbose=False):
         	end tell
         end run'''
         if verbose:
-            runAppleScript(script, verbose=True)
+            try:
+                runAppleScript(script)
+                print("Sent {} spam message{} to {}".format(num, '' if num == 1 else "s", appleIDorPhone))
+                num += 1
+            except KeyboardInterrupt:
+                print("Okay we're done.")
+                sys.exit()
         else:
-            runAppleScript(script)
-
+            try:
+                runAppleScript(script)
+                num += 1
+            except KeyboardInterrupt:
+                print("Okay we're done.")
+                sys.exit()
 
 if __name__ == "__main__":
     options = """OPTIONS:
-    -c <number of messages to send>
-    -v (verbose) 
-    --bible 
-    --random 
-    --from-file <text file>
-        -w  send each word in the specified file
-        -s  send each sentence in the file
-        -l  send each line in the file"""
+    -v (verbose)    prints a tally of spam messages sent
+    -c <number>     specify a set number of messages to send
+    --bible         sends spam from the Word of God
+    --random        random crap spam
+    -w --from-file <text file>     send each word in the specified file
+    -s --from-file <text file>     send each sentence in the file
+    -l --from-file <text file>     send each line in the file"""
     import sys
     import random
     import string
@@ -54,17 +63,17 @@ if __name__ == "__main__":
     elif len(args) >= 3:
         appleID = args[-1]
         verbose = True if "-v" in args else False
-        amount = int(args[args.index("-c") + 1]) if "-c" in args else False
+        numberOfSpams = int(args[args.index("-c") + 1]) if "-c" in args else False
         if spam == "--bible":
-            if amount:
-                sendList(getVerses("theBible.txt")[:amount], appleID, verbose=verbose)
+            if numberOfSpams:
+                sendList(getVerses("theBible.txt")[:numberOfSpams], appleID, verbose=verbose)
             else:
                 sendList(getVerses("theBible.txt"), appleID, verbose=verbose)
         elif spam == "--random":
-            if not amount:
-                amount = random.randint(1, 1000)
+            if not numberOfSpams:
+                numberOfSpams = random.randint(1, 1000)
             messages = []
-            for _ in range(amount):
+            for _ in range(numberOfSpams):
                 message = ''
                 for __ in range(random.randint(10, 300)):
                     message += random.choice(list("\n" + string.ascii_letters + " "))
@@ -73,8 +82,17 @@ if __name__ == "__main__":
         elif spam == "--from-file":
             file = args[args.index("--from-file") + 1]
             if "-w" in args:
-                sendList(getWords(file), appleID, verbose=verbose)
+                if numberOfSpams:
+                    sendList(getWords(file)[:numberOfSpams], appleID, verbose=verbose)
+                else:
+                    sendList(getWords(file), appleID, verbose=verbose)
             elif "-s" in args:
-                sendList(getSentences(file), appleID, verbose=verbose)
+                if numberOfSpams:
+                    sendList(getSentences(file)[:numberOfSpams], appleID, verbose=verbose)
+                else:
+                    sendList(getSentences(file), appleID, verbose=verbose)
             elif "-l" in args:
-                sendList(getLines(file), appleID, verbose=verbose)
+                if numberOfSpams:
+                    sendList(getLines(file)[:numberOfSpams], appleID, verbose=verbose)
+                else:
+                    sendList(getLines(file), appleID, verbose=verbose)
